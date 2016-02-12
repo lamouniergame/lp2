19a490e2fe844e0c5c5e5a3b32be1e83dd41fe50
Afonso <afonso.lamou.vi@gmail.com>
2016-02-12 10:40:01 -0200

    Aula 12/2/16 	new file:   aula1/PJava.java 	new file:   aula1/PJava1.java

---
+++ aula1/PJava.java
@@ -0,0 +1,5 @@
+public class PJava{
+	public static void main(String [] args) {
+		System.out.println("Dibrando o primeiro Programa :D!");
+	}
+}
---
+++ aula1/PJava1.java
@@ -0,0 +1,6 @@
+public class PJava1{
+	public static void main(String [] args){
+		String parametro = args[0];	
+		System.out.println("Valor Digitado :" + parametro);
+	}
+}
