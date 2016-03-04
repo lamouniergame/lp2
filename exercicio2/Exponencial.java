b705cd8bb2fac1d43c5f02e40a14061daf3130c2
lamouniergame <afonso.lamou.vi@gmail.com>
2016-03-04 10:55:44 -0300

    Added files via upload

---
+++ exercicio2/Exponencial.java
@@ -0,0 +1,16 @@
+public class Exponencial{
+    public static void main(String [] args){
+        int i;
+		System.out.print("# ");
+		System.out.print("Q ");
+		System.out.println("C");        
+		for(i = 0;i < 10;i++){
+            System.out.print(+i);
+            System.out.print("-"+(i*i));
+            System.out.println("-"+(i*i*i));
+            System.out.println("");
+        }   
+    }
+}
+
+
