9ddc5465017df62c8a3753872aebd39dfe9e4544
lamouniergame <afonso.lamou.vi@gmail.com>
2016-03-04 09:54:54 -0300

    Update and rename exercicio1_1 to exercicio1_1.java

---
+++ exercicio1/exercicio1_1.java
@@ -0,0 +1,12 @@
+class exercicio1_1 {
+
+	public void imprimir(){
+	System.out.println("O meu nome eh Afonso");
+	}
+
+	public static void main(String [] args){
+	exercicio1_1 ex1 = new exercicio1_1();
+	
+	ex1.imprimir();	
+	}
+}
